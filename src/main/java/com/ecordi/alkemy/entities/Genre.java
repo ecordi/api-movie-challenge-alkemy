005c7953fefa95e194530f0fb3919ab6d6068baf
Eugenio Jesús Cordi Ferreyra <55701@sistemas.frc.utn.edu.ar>
2021-06-10 19:20:20 -0300

    Create Services. Close #5

--- src/main/java/com/ecordi/alkemy/entities/Genre.java
+++ src/main/java/com/ecordi/alkemy/entities/Genre.java
@@ -17,7 +17,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
